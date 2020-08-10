9965fc3d362af0c5213a8797dfcb7e21402abe3c
pavelp21 <pavelvp2182@yandex.ru>
2020-08-10 15:17:00 +0500

    Main page is typeseted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,17 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
+    /* Якорь */
+    $("a[href='#plan']").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
 
 
-
-/*Конец документа*/
+    /*Конец документа*/
 });
