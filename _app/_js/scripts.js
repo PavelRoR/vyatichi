ef773426ab24b58378c276e0f3a96690bea4c164
pavelp21 <pavelvp2182@yandex.ru>
2020-08-11 20:53:29 +0500

    Fonts edits

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -11,7 +11,18 @@ $(document).ready(function () {
             scrollTop: g
         }, 1500)
     });
+    $(".video-wrapper-rev img").click(function () {
+        // if (!$(this).parent().hasClass('switched-rev')) {
+        var a = $(this).parent().attr("data-youtube");
+        // var b = $(this).parent().attr("data-img");
+        // $('#coaches').find('.video-wrapper-rev iframe').parent().html('<img src=' + b + ' alt="Отзыв 1 />"');
+        // $('#coaches').find('.video-wrapper-rev iframe').html('<img src=' + b + ' alt="Отзыв 1 />"');
+        // }
 
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1"  allowfullscreen></iframe>').addClass('switched-rev');
+        // }
+        // else {
+    });
 
     /*Конец документа*/
 });
